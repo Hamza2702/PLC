836eff4f27f173f2f86c27a5acd639db921dec85
Hamza <HamzaKhan2702@outlook.com>
2025-02-03 23:12:24 +0000

    Updated calculator

--- Haskell/calculator.hs
+++ Haskell/calculator.hs
@@ -1,22 +1,22 @@
-data Operator = Add | Subtract | Multiply | Divide
-    deriving Read
-
-eval l o r = case o of
-  "+" -> l + r
-  "-" -> l - r
-  "*" -> l * r
-  "/" -> l / r
-
-prompt txt = do
-  putStrLn txt
-  readLn
+operate :: Double -> Char -> Double -> Double
+operate x '+' y = x + y -- Addition
+operate x '-' y = x - y -- Subtraction
+operate x '*' y = x * y -- Multiplication
+operate x '/' y = if y /= 0 then x / y else error "Can't divide by zero" -- Division
+operate _ _ _   = error "Invalid operator" -- Error
 
 main :: IO ()
-main = 
-    do
-        putStrLn ("Welcome to a calculator project in the big 25!\n")
-        putStrLn("Choose an operation -\n1.\t+\n2.\t-\n3.\t*\n4.\t/")
-        l <- prompt "First number?"
-        o <- prompt "Which operator?"
-        r <- prompt "Second number?"
-        putStrLn $ "The result is " ++ show (eval l o r)
+main = do
+    putStrLn "Enter first number:"
+    numOne <- getLine
+    putStrLn "Enter operator (+, -, *, /):"
+    op <- getChar
+    getChar
+    putStrLn "Enter second number:"
+    numTwo <- getLine
+    
+    let x = read numOne :: Double
+        y = read numTwo :: Double
+        result = operate x op y
+    
+    putStrLn ("Result: " ++ show result)
