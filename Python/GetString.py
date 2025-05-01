4ee3b34d6dd5723fd615166f91afff5fca17cff7
Hamza <114071457+Hamza2702@users.noreply.github.com>
2025-05-01 20:33:46 +0100

    Create GetString.py

---
+++ Python/GetString.py
@@ -0,0 +1,14 @@
+def getString(s, op):
+    if op=="up":
+        return s.upper() # Convert string to uppercase
+    elif op=="low":
+        return s.lower() # Convert string to lowercase
+    elif op=="rev":
+        return s[::-1] ## Reverse string
+    else:
+        return s
+
+def func2(getString, s, op):
+    str = getString(s, op)
+    return str
+print(func2(getString, "good morning", "up"))
